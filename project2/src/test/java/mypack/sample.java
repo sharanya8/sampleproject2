8495afc0c5376989c682032a4486826d8a29c535
ABC <ABC@DESKTOP-S0F2R20>
2021-07-27 20:18:23 +0530

    project2

---
+++ project2/src/test/java/mypack/sample.java
@@ -0,0 +1,19 @@
+package mypack;
+
+import org.testng.annotations.Test;
+
+
+public class  sample 
+	{
+	@Test
+	public void method1()
+	{
+		
+	
+	int  x=30;
+	int y=4000;
+	int z=x+y;
+	System.out.println("output is " +z);
+
+	}
+}
